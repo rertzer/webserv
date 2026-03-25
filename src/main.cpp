a3b7987d3ce839fedb67423a007c12951c9078d1
rertzer <rertzer@student.42.fr>
2026-03-25 10:49:42 +0100

    refactor: main, reordering the functions, variable names in snakecase

--- src/main.cpp
+++ src/main.cpp
@@ -5,66 +5,17 @@
 
 volatile sig_atomic_t quitok = false;
 
-static int checkConfFileExtension(std::string conf_file_name);
-
-void handleBreak(int a) {
-	if (a == SIGINT)
-		quitok = true;
-}
-
-int setSignals() {
-	struct sigaction sigbreak;
-	sigbreak.sa_handler = &handleBreak;
-	sigemptyset(&sigbreak.sa_mask);
-	sigaddset(&sigbreak.sa_mask, SIGTERM);
-	sigbreak.sa_flags = 0;
-	if (sigaction(SIGINT, &sigbreak, NULL) != 0) {
-		std::perror("sigaction");
-		return (1);
-	}
-	return (0);
-}
-
-int checkArgNumber(int ac) {
-	if (ac > 2) {
-		std::cout << "The program can have 1 parameter not more" << std::endl;
-		return (1);
-	}
-	return (0);
-}
-
-std::string getConfFileName(char** av) {
-	std::string conf_file_name;
-	if (av[1]) {
-		conf_file_name = av[1];
-		if (checkConfFileExtension(conf_file_name) != 0) {
-			conf_file_name = "";
-		}
-	} else {
-		conf_file_name = "conf/webserv_3rertzer.conf";
-	}
-	return (conf_file_name);
-}
-
-static int checkConfFileExtension(std::string conf_file_name) {
-	std::string confExtension = ".conf";
-	size_t		extensionPos = conf_file_name.length() - confExtension.length();
-	if (conf_file_name.compare(extensionPos, confExtension.length(), confExtension) != 0) {
-		std::cout << "The program needs a .conf parameter" << std::endl;
-		return (1);
-	}
-	return (0);
-}
+static void		   handleBreak(int a);
+static void		   setSignals();
+static void		   checkArgNumber(int ac);
+static std::string getConfFileName(char** av);
+static void		   checkConfFileExtension(std::string conf_file_name);
 
 int main(int ac, char** av) {
-	if (setSignals() != 0 || checkArgNumber(ac) != 0) {
-		return (1);
-	}
+	setSignals();
+	checkArgNumber(ac);
 
-	std::string conf_file_name = getConfFileName(av);
-	if (conf_file_name.empty()) {
-		return (1);
-	}
+	std::string			conf_file_name = getConfFileName(av);
 	std::vector<Server> serv;
 
 	try {
@@ -77,4 +28,50 @@ int main(int ac, char** av) {
 	}
 	std::cout << "-------------TEST SOCKET------------------" << std::endl << std::endl;
 	return serverRun(serv);
+}
+
+static void setSignals() {
+	struct sigaction sigbreak;
+	sigbreak.sa_handler = &handleBreak;
+	sigemptyset(&sigbreak.sa_mask);
+	sigaddset(&sigbreak.sa_mask, SIGTERM);
+	sigbreak.sa_flags = 0;
+	if (sigaction(SIGINT, &sigbreak, NULL) != 0) {
+		std::perror("sigaction");
+		exit(1);
+	}
+}
+
+static void handleBreak(int a) {
+	if (a == SIGINT) {
+		quitok = true;
+	}
+}
+
+static void checkArgNumber(int ac) {
+	if (ac > 2) {
+		std::cout << "The program can have 1 parameter not more" << std::endl;
+		exit(1);
+	}
+}
+static std::string getConfFileName(char** av) {
+	std::string conf_file_name;
+
+	if (av[1]) {
+		conf_file_name = av[1];
+		checkConfFileExtension(conf_file_name);
+	} else {
+		conf_file_name = "conf/webserv_3rertzer.conf";
+	}
+
+	return (conf_file_name);
+}
+
+static void checkConfFileExtension(std::string conf_file_name) {
+	std::string conf_extension = ".conf";
+	size_t		extension_pos = conf_file_name.length() - conf_extension.length();
+	if (conf_file_name.compare(extension_pos, conf_extension.length(), conf_extension) != 0) {
+		std::cout << "The program needs a .conf parameter" << std::endl;
+		exit(1);
+	}
 }
