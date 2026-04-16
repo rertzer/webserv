e4cbce1b35f7a13f5c09badef8c9d0fabc26dae2
rertzer <rertzer@student.42.fr>
2026-04-16 09:40:46 +0200

    refactor: stringTrim

--- src/socket/stringTrim.cpp
+++ src/socket/stringTrim.cpp
@@ -1,21 +1,23 @@
 #include <string>
 
 void stringTrim(std::string& str) {
-	int pos = str.find_first_not_of(" \t");
-	if (pos != -1 && pos != 0)
+	auto pos = str.find_first_not_of(" \t");
+	if (pos != std::string::npos && pos != 0) {
 		str.erase(0, pos);
+	}
 	pos = str.find_last_not_of(" \t");
-	if (pos != -1 && str.length() && static_cast<unsigned int>(pos) != str.length() - 1)
+	if (pos != std::string::npos && str.length() && pos != str.length() - 1) {
 		str.erase(pos + 1, -1);
+	}
 }
 
 void stringDoubleQuotTrim(std::string& str) {
 	stringTrim(str);
-	int pos = str.find_first_not_of("\"");
-	if (pos != -1 && pos != 0)
+	auto pos = str.find_first_not_of("\"");
+	if (pos != std::string::npos && pos != 0)
 		str.erase(0, pos);
 	pos = str.find_last_not_of("\"");
-	if (pos != -1 && str.length() && static_cast<unsigned int>(pos) != str.length() - 1)
+	if (pos != std::string::npos && str.length() && pos != str.length() - 1)
 		str.erase(pos + 1, -1);
 	if (str == "\"\"")
 		str.clear();
