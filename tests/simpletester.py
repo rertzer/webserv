633e17eef0c83bbcf7b1877fad7666f7228ca2f0
rertzer <rertzer@student.42.fr>
2026-04-29 12:07:12 +0200

    comments

--- tests/simpletester.py
+++ tests/simpletester.py
@@ -9,10 +9,14 @@ class SimpleTester(VirtualRequestTester):
         VirtualRequestTester.__init__(self, index, host, port)
 
     def send_request(self, request):
+        resp = None
         try:
             conn = http.client.HTTPConnection(self.host, self.port)
             conn.request(request.method, request.path, request.content, request.headers)
-            return conn.getresponse()
+            resp =  conn.getresponse()
+            # print(resp.read())
+            # conn.close()
+
         except ConnectionRefusedError as e:
             print({e})
-        return None
+        return resp 
